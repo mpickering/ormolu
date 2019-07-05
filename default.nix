b69f949edce8127254bb41a8773e7dd5d8da95d4
mrkkrp <mark.karpov@tweag.io>
2019-07-05 22:08:18 +0200

    WIP

--- default.nix
+++ default.nix
@@ -3,10 +3,20 @@ let
   gitignoreSource = import ./nix/gitignore { inherit (pkgs) lib; };
   compiler = "ghc864";
   source = gitignoreSource ./.;
+  cabalSrc0 = pkgs.fetchFromGitHub {
+    owner = "haskell";
+    repo = "Cabal";
+    rev = "9616d6892862b7e082e810e2cbe74334767068db";
+    sha256 = "1zj1zaagfnnlwcadlb386cfvvilnc175snz3lcjm8r3vs7i4crwy";
+  };
+  cabalSrc = pkgs.runCommand "extract-cabal-src" {} ''
+    cp -r ${cabalSrc0}/Cabal $out
+  '';
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = (self: super:
     super // {
       "ormolu" = super.callCabal2nix "ormolu" source { };
+      "Cabal" = super.callCabal2nix "Cabal" cabalSrc { };
     });
   };
 in {
